8ed565bfb70de0f8bb7eccc6d7bbf1eff57d6a12
BeBra <sms7client876@gmail.com>
2026-06-11 19:36:17 +0300

    net

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,6 +2,7 @@ import "./header.css"
 import GitHub from "../../../public/img/GitHub.png"
 export default function Header() {
     return (
+        
         <header>
             <div className="information">
                 <p>Hello! I'm</p>
@@ -17,7 +18,42 @@ export default function Header() {
 
             </div>
             <div className="info">
+                
+                
+                <div className="more-info">
+                    <div className="colors">
+                
+                <div className="red"></div>
+                <div className="yellow"></div>
+                <div className="green-block"></div>  
+                
+                </div>
+                    <div className="developer"><p>developer.js</p></div>
+                </div>
+                <pre className="code">
+  <span className="keyword">const</span>
+  
+  {" "}
+  <span className="variable">developer</span> <span className="da">=</span>  {}
+  <span className="orange">{"{"} </span>
 
+  {"\n  "}
+  <span className="purple">name</span><span className="da">:</span>{" "}
+  <span className="green">'Markov Vladimir'</span><span className="da">,</span>
+  {"\n  "}
+  <span className="purple">skills</span><span className="da">:</span><span className="orange">{"["}</span> <span className="green">'React'</span><span className="da">,</span> <span className="green">'HTML/CSS'</span><span className="da">,</span> <span className="green">'JavaScript'</span> <span className="orange">{"]"}</span><span className="da">,</span>
+  {"\n  "}
+  <span className="purple">focuses <span className="da">:</span></span><span className="orange">{"["}</span><span className="green">front-end</span><span className="orange">{"]"}</span>
+  {"\n  "}
+
+
+
+  {"\n"}
+        <span className="orange">{"}"}</span>
+        <span className="da">;</span>
+  
+</pre>
+                
             </div>
         </header>
     )
